31893b3e8bdf244feda199a02e5385112dc6223d
Lara Floor <larafloor3@gmail.com>
2024-11-13 20:43:03 +0100

    bestand tijdelijk.py gecodeerd

---
+++ tijdelijk.py
@@ -0,0 +1,17 @@
+prijzen = {
+    'aardbei': 3,
+    'vanille': 4,
+    'chocolade': 5
+}
+korting = 0.8
+aanbieding = prijzen['aardbei'] * korting
+reclame_tekst = f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts € {aanbieding}"
+reclame_tekst2 = reclame_tekst[:63]
+reclame_tekst3 = reclame_tekst2.upper()
+reclame_tekst4 = reclame_tekst3.split()
+for el in reclame_tekst4:
+    if len(el) > 4:
+        print(el.upper())
+    elif len(el) < 4:
+        print(el.lower())
+print(el)
